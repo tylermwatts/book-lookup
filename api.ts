d9e4de71a21d2f3646779ea6591f1a02f883de88
Warpfox <tylermwatts@gmail.com>
2019-07-21 11:47:32 -0500

    add publisher info

--- api.ts
+++ api.ts
@@ -13,51 +13,49 @@ const api = (app: express.Application) => {
       )
         .then((response: Response) => response.json())
         .catch((err: Error) => console.log(err));
-      const modeledBooks = books.items.map(
-        (b: {
-          volumeInfo: {
-            authors?: string[];
-            title: string;
-            subtitle?: string;
-            description: string;
-            imageLinks: { thumbnail: string };
-            infoLink: string;
-            industryIdentifiers?: [{ type: string; identifier: string }];
-          };
-        }) => {
-          return {
-            author:
-              b.volumeInfo.authors !== undefined ? b.volumeInfo.authors[0] : '',
-            title: b.volumeInfo.title,
-            subtitle: b.volumeInfo.subtitle,
-            description: b.volumeInfo.description,
-            thumbnail: b.volumeInfo.imageLinks
-              ? b.volumeInfo.imageLinks.thumbnail
-              : null,
-            link: b.volumeInfo.infoLink,
-            ISBN: {
-              ISBN_10: b.volumeInfo.industryIdentifiers
-                ? b.volumeInfo.industryIdentifiers.find(
-                    (t: { type: string }) => t.type === 'ISBN_10'
-                  )
-                  ? b.volumeInfo.industryIdentifiers.find(
-                      (t: { type: string }) => t.type === 'ISBN_10'
-                    )!.identifier
-                  : ''
-                : '',
-              ISBN_13: b.volumeInfo.industryIdentifiers
-                ? b.volumeInfo.industryIdentifiers.find(
-                    (t: { type: string }) => t.type === 'ISBN_13'
-                  )
-                  ? b.volumeInfo.industryIdentifiers.find(
-                      (t: { type: string }) => t.type === 'ISBN_13'
-                    )!.identifier
-                  : ''
-                : ''
-            }
-          };
-        }
-      );
+      const modeledBooks = books.items
+        .filter(
+          (a: any) =>
+            a.volumeInfo.authors &&
+            a.volumeInfo.industryIdentifiers &&
+            a.volumeInfo.industryIdentifiers.find(
+              (t: any) => t.type === 'ISBN_10' || t.type === 'ISBN_13'
+            )
+        )
+        .map(
+          (b: {
+            volumeInfo: {
+              authors: string[];
+              title: string;
+              subtitle?: string;
+              publisher?: string;
+              description: string;
+              imageLinks: { thumbnail: string };
+              infoLink: string;
+              industryIdentifiers: [{ type: string; identifier: string }];
+            };
+          }) => {
+            return {
+              author: b.volumeInfo.authors[0],
+              title: b.volumeInfo.title,
+              subtitle: b.volumeInfo.subtitle,
+              publisher: b.volumeInfo.publisher,
+              description: b.volumeInfo.description,
+              thumbnail: b.volumeInfo.imageLinks
+                ? b.volumeInfo.imageLinks.thumbnail
+                : null,
+              link: b.volumeInfo.infoLink,
+              ISBN: {
+                ISBN_10: b.volumeInfo.industryIdentifiers.find(
+                  (t: { type: string }) => t.type === 'ISBN_10'
+                )!.identifier,
+                ISBN_13: b.volumeInfo.industryIdentifiers.find(
+                  (t: { type: string }) => t.type === 'ISBN_13'
+                )!.identifier
+              }
+            };
+          }
+        );
       res.json({
         books: modeledBooks
       });
--- client/src/types/Book.ts
+++ client/src/types/Book.ts
@@ -2,11 +2,12 @@ export type Book = {
   author: string;
   title: string;
   subtitle?: string;
+  publisher?: string;
   description: string;
   thumbnail?: string;
   link: string;
   ISBN: {
-    ISBN_10?: string;
-    ISBN_13?: string;
+    ISBN_10: string;
+    ISBN_13: string;
   };
 };
