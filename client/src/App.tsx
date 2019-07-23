0d7a458763f815dcb3ccdb4e707df30c832aebea
Warpfox <tylermwatts@gmail.com>
2019-07-23 15:39:20 -0500

    add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
 import Container from './Components/Container/Container';
+import NavHeader from './Components/NavHeader/NavHeader';
 
 function App() {
-  return <Container />;
+  return (
+    <Router>
+      <NavHeader />
+      <div>
+        <Route exact path="/" component={Container} />
+        <Route path="/about/" component={About} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
