028f6c4e3f741b535d211dc6eb193cb9b1c86aeb
Warpfox <tylermwatts@gmail.com>
2019-08-01 20:23:22 -0500

    heroku setup

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,19 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import apiConfig from './api';
 import express = require('express');
+import path = require('path');
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app: express.Application = express();
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 apiConfig(app);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(port || 3001, () => {
   console.log(`Server running on http://localhost:${port} ...`);
